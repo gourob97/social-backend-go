6bdc9ae1e8a2c9f9c2f8f5cc20f10f964a002b0d
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2025-11-23 18:06:12 +0600

    changed post request payload model

--- internal/dto/post_dto.go
+++ internal/dto/post_dto.go
@@ -3,18 +3,15 @@ package dto
 import "time"
 
 type CreatePostRequest struct {
-	Title   string `json:"title" validate:"required,min=1,max=200"`
 	Content string `json:"content" validate:"required,min=1,max=10000"`
 }
 
 type UpdatePostRequest struct {
-	Title   string `json:"title" validate:"omitempty,min=1,max=200"`
 	Content string `json:"content" validate:"omitempty,min=1,max=10000"`
 }
 
 type PostResponse struct {
 	ID        uint      `json:"id"`
-	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	UserID    uint      `json:"user_id"`
 	Username  string    `json:"username"`
